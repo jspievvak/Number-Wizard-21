a483fbde33336f875a2c6bad2480bfef51fc8747
DESKTOP-5PKPSGL\ADMIN <jspievvak@gmail.com>
2021-01-27 23:30:38 +0100

    Number of guesses added

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -8,13 +8,9 @@ public class NumberWizard : MonoBehaviour
     int min;
     int max;
     int guess;
+    int guessesnumber;
 
-    void NextGuess()
-    {
-        guess = (min + max) / 2;  
-        Debug.Log("Is your number greter or smaller than " + guess + " ?");
-                     
-    }
+    //code
 
     void StartGame()
     {
@@ -22,11 +18,12 @@ public class NumberWizard : MonoBehaviour
         max = 1000;
         guess = (min + max) / 2;
         Debug.Log("Welcome to Number Wizard!!!");
-        print("Pick a number between " + min + " do " + max);
+        print("Pick a number between " + min + " to " + max);
         Debug.Log("Let me know if your number is greater or smaller than " + guess);
-        print("Arrow Up means your number is higher, Arrow Down - lower. If I guessed, push Enter");
+        print("Arrow Up means your number is higher, Arrow Down - lower. If I guessed, press Enter");
         max++;
     }
+
 
     void Start()
     {
@@ -36,6 +33,9 @@ public class NumberWizard : MonoBehaviour
 
     void Update()
     {
+
+        //play
+
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             min = guess;
@@ -48,10 +48,35 @@ public class NumberWizard : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            Debug.Log("I won. I always do.");
+            Debug.Log("I won. I always do. I did it in " + guessesnumber + " guesses!" + " To play again, press R.");
+
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ReloadScene();
+        }
+
+        //code 
+
+        void ReloadScene()
+        {
             Scene scene = SceneManager.GetActiveScene();
             SceneManager.LoadScene(scene.name);
         }
+
+        void NextGuess()
+        {
+            guess = (min + max) / 2;
+            Debug.Log("Is your number " + guess + " ?");
+
+            guessesnumber++;
+
+        }
+   
+        
+      
+
+
 
         // poprawic buga z wybieraniem liczby 1000 //////// zrobione
         // sprobowac samodzielnie zamienic kolory komunikatow w konsoli 
